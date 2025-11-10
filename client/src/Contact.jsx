413487148e9f6670bbc70c0158986d7d988b1a23
RJones-Code <russj065@gmail.com>
2025-11-10 10:51:40 -0500

    Changes made for Map

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -1,11 +1,13 @@
 import AILabHeader from "./components/pagenavbar";
 import { Container } from "react-bootstrap";
+import MapEmbed from "./components/embeddedMap";
 
 function Contact() {
     return (
         <Container fluid>
             <AILabHeader/>
             <h1>Contact Page</h1>
+            <MapEmbed/>
         </Container>
 
     );
