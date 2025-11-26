546792bdb610bb3b261096c705944c8b5df9ddc3
Kriston Theng <kristontheng@gmail.com>
2025-11-25 21:42:20 -0500

    Add allowedHeaders to middleware

--- server/middleware/corsOptions.js
+++ server/middleware/corsOptions.js
@@ -1,7 +1,8 @@
 
 const corsOptions = {
     origin: "https://drclaireslee.vercel.app",
-    methods: ["GET", "POST", "PATCH", "DELETE"]
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-auth"],
 };
 
 export default corsOptions;
