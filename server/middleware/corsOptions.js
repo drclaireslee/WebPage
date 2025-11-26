e29edc4d36d31b8f7e68e76f697308dbb3471f18
Kriston Theng <kristontheng@gmail.com>
2025-11-25 21:42:57 -0500

    Fixed Syntax in allowedHeaders

--- server/middleware/corsOptions.js
+++ server/middleware/corsOptions.js
@@ -2,7 +2,7 @@
 const corsOptions = {
     origin: "https://drclaireslee.vercel.app",
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "HEAD"],
-    allowedHeaders: ["Content-Type", "Authorization", "x-auth"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-auth"]
 };
 
 export default corsOptions;
