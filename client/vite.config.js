e11423e476332f0ec349b73116e2e05c154d8b00
RJones-Code <russj065@gmail.com>
2025-12-02 12:22:48 -0500

    Revert Changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://drclaireslee-backend.vercel.app",
+        target: "https://drclaireslee.vercel.app",
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path.replace(/^\/api/, "/api")
