f4f4260e1853fd5e2387ea7f982f8036b8693062
RJones-Code <russj065@gmail.com>
2025-12-03 16:24:20 -0500

    Change Cors to allow Local Development

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,5 +1,17 @@
+
+const allowedOrigins = [
+  "https://drclaireslee.vercel.app",
+  "http://localhost:5173",
+];
+
 const corsOptions = {
-    origin: "https://drclaireslee.vercel.app" | "*",
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+        } else {
+        callback(new Error("CORS blocked"));
+        }
+    },
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "HEAD"],
     allowedHeaders: ["Content-Type", "Authorization", "x-auth"],
 };
