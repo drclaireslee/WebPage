31802369bb12075ab6d59ddecbc9c32a02d7c895
AmazingConnie <rmallu21@gmail.com>
2025-12-05 00:12:49 -0500

    addition of research papers to research page + contact form added.

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -1,5 +1,5 @@
 import AILabHeader from "./components/pagenavbar";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import MapEmbed from "./components/embeddedMap";
 import Footer from "./components/footer";
 
@@ -9,30 +9,40 @@ function Contact() {
             <AILabHeader/>
            
             <Container fluid className="p-5 bg-white">
-                 <h1>Contact Us</h1>
-             
-
-                   <address>
-	            <p className="text-start fs-5">
-		            Email: <a href="mailto:claire_lee@uml.edu">claire_lee@uml.edu</a> <br/>
-					Address: <br/>
-					113 Wilder Street<br/>
-					Donna Manning School of Social and Health Sciences Building <br/>
-                    School of Criminology and Justice Studies <br/>
-                    University of Massachusetts Lowell <br/>
-					Lowell, MA 01859
-				</p>
-        	    </address>  
+                 <h1>Contact Me</h1>
+                 
+                        <address>
+                        <p className="text-start fs-5 align-items-center">
+                            Email: <a href="mailto:claire_lee@uml.edu">claire_lee@uml.edu</a> <br/>
+                            Address: <br/>
+                            113 Wilder Street<br/>
+                            Donna Manning School of Social and Health Sciences Building <br/>
+                            School of Criminology and Justice Studies <br/>
+                            University of Massachusetts Lowell <br/>
+                            Lowell, MA 01859
+                        </p>
+        	            </address>  
+                    
+                  
+                        
+                 
+                   
 
          
                
                 
             </Container>
 
-            <Container fluid className="bg-white">
+            <Container fluid className="p-2 bg-white d-flex align-items-center justify-content-center">
                 <MapEmbed/>
+                
             </Container>
             
+            <Container fluid className="p-5 bg-white">
+                
+                <iframe width="640px" height="480px" src="https://nam10.safelinks.protection.outlook.com/?url=https%3A%2F%2Fforms.office.com%2Fr%2FQSANdPncGm%3Fembed%3Dtrue&data=05%7C02%7CZuriel_Pagan%40student.uml.edu%7C8f2ffb697d014858d8f408de31e06bff%7C4c25b8a617f746f983f054734ab81fb1%7C0%7C0%7C639003037110466043%7CUnknown%7CTWFpbGZsb3d8eyJFbXB0eU1hcGkiOnRydWUsIlYiOiIwLjAuMDAwMCIsIlAiOiJXaW4zMiIsIkFOIjoiTWFpbCIsIldUIjoyfQ%3D%3D%7C0%7C%7C%7C&sdata=xAr5sRuomIP4HVA6P%2F%2FgJ8XMKM63PA%2FpgzV%2Bqh7%2B6vE%3D&reserved=0" frameBorder="0" marginWidth="0" marginHeight="0" style={{border: 'none', maxWidth:'100%', maxHeight:'100vh'}} allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" msallowfullscreen="true"> </iframe>
+
+            </Container>
             {/**Commented out due to formatting issues */}
             <Footer/>
         </div>
