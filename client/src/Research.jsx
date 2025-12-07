47d5c4ff7b9b6d797becdcdfde6f7535dd5c38f5
AmazingConnie <rmallu21@gmail.com>
2025-12-06 23:54:57 -0500

    undo deletion of published articles section

--- client/src/Research.jsx
+++ client/src/Research.jsx
@@ -63,7 +63,7 @@ function Research() {
               </Container>
 
               {/*Links to author's published books */}
-              <Container fluid className="p-5 bg-white" role="section" aria-label="published books and articles">
+              <Container fluid className="p-5 bg-white" role="section" aria-label="published books">
                 <h1>Books</h1>
                 
                 <Row className="p-4" role="row">
@@ -80,12 +80,16 @@ function Research() {
                     <a href="https://product.kyobobook.co.kr/detail/S000216868690" className="fs-5">Available on Kyobo</a>
                   </Col>
                 </Row>
-
-                {/*Link to Peer-Reviewed Articles */}
-                <h1>Articles</h1>
-                <a href="https://scholar.google.com/citations?hl=en&user=PABcsxAAAAAJ&view_op=list_works&sortby=pubdate&inst=8587491165479738887" className="fs-3">Link to Google Scholar</a>
        
               </Container>  
+
+              
+              {/*Link to Peer-Reviewed Articles */}
+              <Container fluid className="p-5 bg-white" role="section" aria-label="published articles">
+                <h1>Articles</h1>
+                <a href="https://scholar.google.com/citations?hl=en&user=PABcsxAAAAAJ&view_op=list_works&sortby=pubdate&inst=8587491165479738887" className="fs-3">Link to Google Scholar</a>
+                
+              </Container>
 
               <Footer/>
 
